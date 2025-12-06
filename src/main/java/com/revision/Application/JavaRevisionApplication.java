fd45b22b2efaa074894e810913746068954c0b49
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2025-12-06 12:35:02 +0100

    Kotlin -> Java

---
+++ src/main/java/com/revision/Application/JavaRevisionApplication.java
@@ -0,0 +1,13 @@
+package com.revision.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRevisionApplication.class, args);
+	}
+
+}
