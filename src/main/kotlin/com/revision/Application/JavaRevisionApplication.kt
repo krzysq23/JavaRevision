6872e20d8c395644af51a712c2140a57af87ae66
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2025-12-06 12:32:41 +0100

    initial empty spring boot project

---
+++ src/main/kotlin/com/revision/Application/JavaRevisionApplication.kt
@@ -0,0 +1,11 @@
+package com.revision.Application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaRevisionApplication
+
+fun main(args: Array<String>) {
+	runApplication<JavaRevisionApplication>(*args)
+}
