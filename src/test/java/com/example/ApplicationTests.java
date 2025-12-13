55fe65707e69d9221934e54fafad13ef0bdc8488
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2025-12-13 15:25:01 +0100

    gradle example

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -1,8 +1,10 @@
+package com.example;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaRevisionApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
